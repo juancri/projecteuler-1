071bc7e77557c64acc07387c8db35051a2cdb4c0
Juan C. Olivares <cristobal@cxsoftware.com>
2014-10-10 15:06:09 -0300

    First commit

---
+++ euler1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class MainClass
+{
+	public static void Main ()
+	{
+		var res = Enumerable.Range (1, 999)
+			.Where (x => x % 3 == 0 || x % 5 == 0)
+			.Sum ();
+
+		Console.WriteLine (res);
+	}
+}
